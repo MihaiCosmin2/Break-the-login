6790cc52efaa0cab949e81713bf6bbb3c6b2966e
Mihai Cosmin <serbanoiumihai2@gmail.com>
2026-05-23 08:00:13 +0300

    Adaugat aplicatie securizata

--- app_securizat.py
+++ app_securizat.py
@@ -2,15 +2,33 @@
 from flask import Flask, request, render_template_string, redirect, url_for, session, make_response
 import sqlite3
 import os
+import re
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
+import bcrypt
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from datetime import timedelta
+
 
 app = Flask(__name__)
 
-# cheie vulnerabila
-app.secret_key = "parola_simpla" 
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=["100 per day", "20 per hour"],
+    storage_uri="memory://"
+)
 
-app.config["SESSION_COOKIE_HTTPONLY"] = False
-app.config["SESSION_COOKIE_SECURE"] = False
-app.config["SESSION_COOKIE_SAMESITE"] = None
+
+app.secret_key = "Very_PowerfulPasswordIWasBornIn2002" 
+
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SAMESITE"] = 'Lax'
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=35)
+
+
+serializer = URLSafeTimedSerializer(app.secret_key)
 
 # functie pentru conectarea la baza de date
 def acces_db():
@@ -28,7 +46,7 @@ def home():
                 <li><a href="/tickets">Tichete CRUD</a></li>
                 <li><a href="/logout">Iesire din cont</a></li>
             </ul> """, id=session['uid'], email=session.get('user_email'))
-    return '<h1>Deskly versiune vulnerabila</h1><a href="/login">Login</a> | <a href="/register">Creaza Cont</a>'
+    return '<h1>Deskly versiune securizata</h1><a href="/login">Login</a> | <a href="/register">Creaza Cont</a>'
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -39,11 +57,24 @@ def inregistrare():
         
         # 4.1: Password Policy slab - Sunt acceptate parole foarte scurte sau triviale – Nu există validare la înregistrare
         # 4.2: Stocare nesigura a parolelor - Parole stocate în clar – Sau hash slab (MD5/SHA1)
+        ######
+        if len(parola) < 9:
+            return "Parola trebuie sa aiba minim 9 caractere"
+        if not re.search(r"[A-Z]", parola):
+            return "Parola trebuie sa contina cel putin o litera mare"
+        if not re.search(r"[a-z]", parola):
+            return "Parola trebuie sa contna cel putin o litera mica"
+        if not re.search(r"[0-9]", parola):
+            return "Parola trebuie sa contina cel putin o cifra"
+        ######
+
+        salt = bcrypt.gensalt()
+        parola_hashed = bcrypt.hashpw(parola.encode('utf-8'), salt).decode('utf-8')
 
         db = acces_db()
         try:
             db.execute('INSERT INTO users (email, password_hash, role) VALUES (?, ?, ?)', 
-                       (mail, parola, 'ANALYST'))
+                       (mail, parola_hashed, 'ANALYST'))
             db.commit()
             return redirect('/login')
         except:
@@ -61,43 +92,44 @@ def inregistrare():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("10 per minute", error_message="Incearca mai tarziu.")
 def login():
     if request.method == 'POST':
         email_introdus = request.form.get('email')
         pass_introdus = request.form.get('password')
         # 4.3: Brute force / lipsă rate limiting: Număr nelimitat de încercări login – Fără blocare cont
         db = acces_db()
-
-        query = f"SELECT * FROM users WHERE email = '{email_introdus}'"
         
         try:
-            om = db.execute(query).fetchone()
+            om = db.execute('SELECT * FROM users WHERE email = ?', (email_introdus,)).fetchone()
         except:
             db.close()
-            return "Eroare, sql injection posibil"
+            return "Eroare baza de date."
 
         # 4.4: User Enumeration - mesaje care ajuta atacatorul
+        mesaj_eroare = "Autentificare esuata: Email sau parola incorecta."
         if not om:
             db.close()
-            return "USER_NOT_FOUND: Acest email nu exista in baza de date."
+            return mesaj_eroare
         
-        if om['password_hash'] == pass_introdus:
-            # 4.5: Gestionare nesigura a sesiunilor
+        hash_din_db = om['password_hash'].encode('utf-8')
+        if bcrypt.checkpw(pass_introdus.encode('utf-8'), hash_din_db):
+            session.permanent = True
             session['uid'] = om['id']
             session['user_email'] = om['email']
             session['role'] = om['role']
 
-            db.execute('INSERT INTO audit_logs (user_id, action, resource, ip_address) VALUES (?, ?, ?, ?)',(om['id'], 'LOGIN', 'auth', request.remote_addr))
+            db.execute('INSERT INTO audit_logs (user_id, action, resource, ip_address) VALUES (?, ?, ?, ?)',
+                       (om['id'], 'LOGIN', 'auth', request.remote_addr))
             db.commit()
             db.close()
-
             return redirect('/')
         else:
             db.close()
-            return "WRNG_PASS: Parola introdusa este incorecta."
+            return mesaj_eroare
 
     return '''
-        <h4>Deskly versiune vulnerabila</h4>
+        <h4>Deskly versiune securizata</h4>
         <form method="post">
             Email: <input name="email"><br>
             Parola: <input name="password" type="password"><br>
@@ -123,7 +155,7 @@ def list_tickets():
                    (session['uid'], 'CREATE_TICKET', 'tickets'))
         db.commit()
 
-    bilete = db.execute('SELECT * FROM tickets').fetchall()
+    bilete = db.execute('SELECT * FROM tickets WHERE owner_id = ?', (session['uid'],)).fetchall()
     db.close()
     
     html = '''
@@ -145,10 +177,13 @@ def list_tickets():
 @app.route('/forgot', methods=['GET', 'POST'])
 def forgot():
     if request.method == 'POST':
-        # 4.6: Resetare parola nesigura - Token predictibil, reutilizabil, fara expirare
-        import time
-        token_slab = str(int(time.time())) 
-        return f"Daca userul exista, s-a trimis link-ul. (Debug: token-ul tau este {token_slab})"
+        email = request.form.get('email')
+        
+        token_sigur = serializer.dumps(email, salt='resetare-parola-salt')
+        
+        print(f"\n(EMAIL) Catre: {email}\nLink resetare: http://127.0.0.1:5000/reset?token={token_sigur}\n")
+        
+        return "Daca adresa de email exista, vei primi un link."
     
     return 'Email: <form method="post"><input name="email"><button>Trimite token</button></form>'
 
@@ -160,29 +195,38 @@ def logout():
 
 @app.route('/reset', methods=['GET', 'POST'])
 def reset_password():
+    token_introdus = request.args.get('token') or request.form.get('token')
+    
+    if not token_introdus:
+        return "Error: Lipseste token-ul de resetare."
+
+    try:
+        email = serializer.loads(token_introdus, salt='resetare-parola-salt', max_age=900)
+    except SignatureExpired:
+        return "ERROR: Token-ul a expirat! Te rog cere altul."
+    except BadSignature:
+        return "ERROR: Token invalid sau manipulat."
+
     if request.method == 'POST':
-        email = request.form.get('email')
-        token_introdus = request.form.get('token')
         parola_noua = request.form.get('password')
         
-        #4.6: Resetarea parolei fara control corespunzator.
+        salt = bcrypt.gensalt()
+        parola_noua_hashed = bcrypt.hashpw(parola_noua.encode('utf-8'), salt).decode('utf-8')
         
         db = acces_db()
-        db.execute('UPDATE users SET password_hash = ? WHERE email = ?', (parola_noua, email))
+        db.execute('UPDATE users SET password_hash = ? WHERE email = ?', (parola_noua_hashed, email))
         db.commit()
         db.close()
         
-        return f"Parola contului {email} a fost schimbata cu success!"
+        return f"Parola a fost schimbata cu succes."
 
-    return '''
+    return f'''
         <h3>Setare Parola Noua</h3>
         <form method="post">
-            Email: <input name="email"><br>
-            Token: <input name="token"><br>
+            <input type="hidden" name="token" value="{token_introdus}">
             Parola Noua: <input name="password" type="password"><br>
             <button type="submit">Schimba Parola</button>
-        </form>
-    '''
+        </form>'''
 
 
 if __name__ == '__main__':
--- brute_force.py
+++ brute_force.py
@@ -2,17 +2,20 @@ import requests
 
 url = "http://127.0.0.1:5000/login" 
 
-parole = ["admin", "password", "pass1", "123", "12345", "parola", "1"]
+parole = ["admin", "password", "123", "12345", "parola", "pass1", "1"]
 
 for parola in parole:
     print(f"Incearca parola: {parola}")
     
     payload = {
-        "email": "cosmin2@gmail.com", 
+        "email": "cosmin4@gmail.com", 
         "password": parola}
     
     raspuns = requests.post(url, data=payload)
     
+    if raspuns.status_code == 429:
+        print(f"Atac nereusit, blocat de server!")
+        break
     
     if "USER_NOT_FOUND" not in raspuns.text and "WRNG_PASS" not in raspuns.text:
         print(f"\n Parola gasita a contului: {parola}")
